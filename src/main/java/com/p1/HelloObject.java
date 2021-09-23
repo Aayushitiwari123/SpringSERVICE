38db8b8fa8dd21b5e9436b10c1d8e89452534351
Aayushitiwari123 <tiwariaayushi50@gmail.com>
2021-09-23 11:27:33 -0700

    HelloObject

--- src/main/java/com/p1/HelloObject.java
+++ src/main/java/com/p1/HelloObject.java
@@ -1,5 +1,14 @@
 package com.p1;
 
 public class HelloObject {
+private String message;
+
+public String getMessage() {
+	return message;
+}
+
+public void setMessage(String message) {
+	this.message = message;
+}
 
 }
